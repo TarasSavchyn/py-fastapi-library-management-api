9e00544878c63abb919408ccfb6b58d67aa080f5
Taras Savchyn <savik1992@gmail.com>
2024-03-31 20:43:48 +0300

    create models.py with models Author&Book

---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    bio = Column(String)
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    summary = Column(String)
+    publication_date = Column(Date)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    author = relationship("Author", back_populates="books")
