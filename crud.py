a9a1e57112f862b2a5958e02f63827b78c0f7021
Taras Savchyn <savik1992@gmail.com>
2024-04-01 21:01:11 +0300

    add type annotations in crud.py

--- crud.py
+++ crud.py
@@ -1,27 +1,47 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 import models
 import schemas
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
-    db_author = models.Author(name=author.name, bio=author.bio)
+def create_author(
+        db: Session,
+        author: schemas.AuthorCreate
+) -> models.Author:
+    db_author = models.Author(
+        name=author.name,
+        bio=author.bio
+    )
     db.add(db_author)
     db.commit()
     db.refresh(db_author)
     return db_author
 
 
-def get_author(db: Session, author_id: int):
+def get_author(
+        db: Session,
+        author_id: int
+) -> models.Author:
     return db.query(models.Author).filter(
         models.Author.id == author_id
     ).first()
 
 
-def get_authors(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Author).offset(skip).limit(limit).all()
+def get_authors(
+        db: Session,
+        skip: int = 0,
+        limit: int = 10
+) -> List[models.Author]:
+    return db.query(
+        models.Author
+    ).offset(skip).limit(limit).all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(
+        db: Session,
+        book: schemas.BookCreate
+) -> models.Book:
     db_book = models.Book(**book.dict())
     db.add(db_book)
     db.commit()
@@ -29,12 +49,23 @@ def create_book(db: Session, book: schemas.BookCreate):
     return db_book
 
 
-def get_book(db: Session, book_id: int):
-    return db.query(models.Book).filter(models.Book.id == book_id).first()
+def get_book(
+        db: Session,
+        book_id: int
+) -> models.Book:
+    return db.query(models.Book).filter(
+        models.Book.id == book_id
+    ).first()
 
 
-def get_books(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Book).offset(skip).limit(limit).all()
+def get_books(
+        db: Session,
+        skip: int = 0,
+        limit: int = 10
+) -> List[models.Book]:
+    return db.query(
+        models.Book
+    ).offset(skip).limit(limit).all()
 
 
 def get_books_by_author(
@@ -42,7 +73,7 @@ def get_books_by_author(
         author_id: int,
         skip: int = 0,
         limit: int = 10
-):
+) -> List[models.Book]:
     return (
         db.query(models.Book)
         .filter(models.Book.author_id == author_id)
